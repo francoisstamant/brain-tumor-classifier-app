09745572e8e7aab2400afd4644b7005d752a4042
François St-Amant <56452799+francoisstamant@users.noreply.github.com>
2020-07-13 15:59:59 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,130 @@
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+from dash import no_update
+import base64
+import io
+from io import BytesIO
+import re
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, Conv3D, BatchNormalization, Activation
+from keras import backend as K
+import os
+from PIL import Image
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+import pandas as pd
+from keras.models import load_model
+
+classes = ['glioma_tumor', 'meningioma_tumor', 'no_tumor', 'pituitary_tumor']
+
+enc = OneHotEncoder()
+enc.fit([[0], [1], [2], [3]]) 
+def names(number):
+    if(number == 0):
+        return 'a Glioma tumor'
+    elif(number == 1):
+        return 'a Meningioma tumor'
+    elif(number == 2):
+        return 'no tumor'
+    elif(number == 3):
+        return 'a Pituitary tumor'  
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+app.layout = html.Div([
+    
+    html.H1(children='BRAIN TUMOR CLASSIFIER', style={'textAlign': 'center'
+        }),
+    
+    
+    dcc.Markdown('''
+                ###### Step 1: Import a single image using upload button
+                ###### Step 2: Wait for prediction to appear
+    '''),
+    
+    
+    dcc.Upload(
+        id='upload-image',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '95%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        }, 
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-image-upload', style={'position':'absolute', 'left':'200px', 'top':'250px'}),
+    
+    html.Div(id='prediction', style={'position':'absolute', 'left':'950px', 'top':'390px', 'font-size':'x-large'}),
+    html.Div(id='prediction2', style={'position':'absolute', 'left':'950px', 'top':'435px', 'font-size': 'x-large'}),
+
+])
+
+def parse_contents(contents):
+    
+    return html.Img(src=contents, style={'height':'450px', 'width':'450px'})
+
+
+@app.callback([Output('output-image-upload', 'children'), Output('prediction', 'children'), Output('prediction2', 'children')],
+              [Input('upload-image', 'contents')])
+
+def update_output(list_of_contents):        
+    
+    if list_of_contents is not None:
+        children = parse_contents(list_of_contents[0]) 
+         
+        img_data = list_of_contents[0]
+        img_data = re.sub('data:image/jpeg;base64,', '', img_data)
+        img_data = base64.b64decode(img_data)  
+        
+        stream = io.BytesIO(img_data)
+        img_pil = Image.open(stream)
+        
+        
+        #Load model, change image to array and predict
+        model = load_model('model_final.h5') 
+        dim = (150, 150)
+        
+        img = np.array(img_pil.resize(dim))
+        
+        x = img.reshape(1,150,150,3)
+
+        answ = model.predict(x)
+        classification = np.where(answ == np.amax(answ))[1][0]
+        pred=str(round(answ[0][classification]*100 ,3)) + '% confidence there is ' + names(classification)   
+        
+        
+        if classification != 2:
+            no_tumor = str(round(answ[0][2]*100 ,3))
+            pred2 = no_tumor + '% confidence there is no tumor'
+        else:
+            pred2 = None
+        
+        return children, pred, pred2
+    
+    else:
+        return (no_update, no_update, no_update)      
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
